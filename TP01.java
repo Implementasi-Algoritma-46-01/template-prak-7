fc2045b674246e6502059ad6c0ee1e15f136b4a6
rizzamandasari <valgard.aegist@gmail.com>
2022-11-06 09:51:08 +0700

    autograd

--- TP01.java
+++
@@ -1,6 +0,0 @@
-public class TP01 {
-
-	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
-	}
-}
--- TP02.java
+++
@@ -1,6 +0,0 @@
-public class TP02 {
-
-	public static void main(final String[] args) {
-		// Kerjakan soalnya di sini
-	}
-}
